64c1d6e721a0737dcef9a2296782f3590b6c538c
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-03-13 19:49:21 -0300

    first CSV reading file working and throwing

---
+++ class-01/index.test.js
@@ -0,0 +1,17 @@
+const { error } = require('./src/constants')
+const File = require("./src/file");
+const assert = require("assert");
+
+// IFEE
+; (async () => {
+
+  {
+    const filePath = "./mocks/emptyFile-invalid.csv";
+    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJSON(filePath);
+
+    await assert.rejects(result, expected);
+  }
+
+
+})();
