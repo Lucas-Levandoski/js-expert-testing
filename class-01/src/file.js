6b97496b1ba2836cf12043250b3083164c53371d
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-03-13 20:08:24 -0300

    first test working

--- class-01/src/file.js
+++ class-01/src/file.js
@@ -1,11 +1,30 @@
 const { readFile } = require('fs/promises');
+const { error } = require('./constants');
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"],
+}
 
 
 class File {
   static async csvToJSON(filePath) {
     const content = await readFile(filePath, "utf-8");
+    const { error, valid } = this.isValid(content)
 
-    console.log({ content });
+    if (!valid) throw new Error(error);
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTIONS) {
+    let [headers, ...rows] = csvString.split(/\r?\n/);
+
+    rows = rows.filter(Boolean);
+
+    if (!rows.length) {
+      return ({
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      })
+    }
   }
 }
 
